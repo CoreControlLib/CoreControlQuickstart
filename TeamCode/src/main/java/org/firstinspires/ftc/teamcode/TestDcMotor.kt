1725734a0e480e2caaae3de4cfd6fbe8d7d9c8a8
Andrei Durlea <andrei.durlea.telefon@gmail.com>
2026-02-27 23:07:30 +0200

    Updated motor test opmodes
    They now browse through the hardware map directly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDcMotor.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDcMotor.kt
@@ -1,35 +1,80 @@
 package org.firstinspires.ftc.teamcode
 
-import com.andreidurlea.corecontrol.buttonsFeature.AnalogButton
+import com.andreidurlea.corecontrol.buttonsFeature.Button
 import com.andreidurlea.corecontrol.miscellaneous.telemetryEverywhere
 import com.andreidurlea.corecontrol.opModesFeature.CoreTeleOp
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior
 
-@TeleOp(name = "Test Motor", group = "Testing")
+@TeleOp(name = "Test DcMotor", group = "Testing")
 class TestDcMotor : CoreTeleOp() {
 
-    lateinit var motor: DcMotor
+    lateinit var motors: List<DcMotor>
+    var currentMotorIndex = 0
+    var direction = 1.0
 
-    var motorName = "EDIT THIS TO CHOOSE YOUR SERVO NAME FROM THE CONFIG"
-    // you can find and edits configs in TeamCode/res/xml
-    // or in the Driver Station in the three dots menu > "configure robot"
+    val reverseDirection = Button({ gamepad1.right_bumper })
 
-    val spin = AnalogButton({ gamepad1.right_trigger })
-    val spinInReverse = AnalogButton({ gamepad1.left_trigger })
+    val nextMotor = Button({ gamepad1.dpad_right })
+    val prevMotor = Button({ gamepad1.dpad_left })
 
     override fun onInit() {
-        motor = hardwareMap[DcMotor::class.java, motorName]
-        motor.direction = DcMotorSimple.Direction.FORWARD // Starts robot in forward (CW) direction
+        motors = hardwareMap.getAll(DcMotor::class.java)
+        motors.forEach { it.zeroPowerBehavior = ZeroPowerBehavior.FLOAT }
+        telemetryEverywhere.addLine("To add a motor in your config, see the /TeamCode/res/xml folder" +
+                ", or, tap the three dot menu on your driver station and select 'Configure robot'")
     }
 
     override fun onLoop() {
-        telemetryEverywhere.addLine("Spin (Clowckwise) == Right Trigger (0-100%)")
-        telemetryEverywhere.addLine("Spin (Counterclockwise) == Left Trigger (0-100%)")
+        if (motors.isEmpty()) {
+            telemetryEverywhere.addLine("No DC motors found.")
+            return
+        }
 
-        telemetryEverywhere.addData("Currently testing", motorName)
+        val previousMotorIndex = currentMotorIndex
+        if (nextMotor.wasPressed()) {
+            currentMotorIndex = (currentMotorIndex + 1) % motors.size
+        }
+        if (prevMotor.wasPressed()) {
+            currentMotorIndex = (currentMotorIndex - 1 + motors.size) % motors.size
+        }
+        if (previousMotorIndex != currentMotorIndex) {
+            motors[previousMotorIndex].power = 0.0
+        }
 
-        motor.power = spin.value - spinInReverse.value
+
+        val currentMotor = motors[currentMotorIndex]
+        val motorName = hardwareMap.getNamesOf(currentMotor).firstOrNull() ?: "Unknown"
+
+        if (reverseDirection.wasPressed()) {
+            direction *= -1.0
+        }
+
+        telemetryEverywhere.addLine("[INFO] This OpMode can find and test any motor in your config")
+        telemetryEverywhere.addLine("[INFO] Control power with Right Trigger")
+        telemetryEverywhere.addLine("[INFO] Reverse direction with Right Bumper")
+        telemetryEverywhere.addLine("[INFO] Hold Left Bumper to brake the motor")
+        telemetryEverywhere.addLine("[INFO] Cycle through motors in your config == D-Pad Left/Right")
+        telemetryEverywhere.addLine("====================================")
+        telemetryEverywhere.addData("Currently testing", "MOTOR ${currentMotorIndex + 1}/${motors.size}, named ${motorName.uppercase()}")
+        telemetryEverywhere.addData("Motor Power", currentMotor.power)
+        telemetryEverywhere.addData("Motor Direction",
+            if (direction > 0) "FORWARD (Positive power spins clockwise)"
+            else "REVERSE (Negative power spins counter-clockwise)")
+        telemetryEverywhere.addLine("====================================")
+
+        if (gamepad1.left_bumper) {
+            currentMotor.power = 0.0
+            currentMotor.zeroPowerBehavior = ZeroPowerBehavior.BRAKE
+        } else {
+            currentMotor.zeroPowerBehavior = ZeroPowerBehavior.FLOAT
+            val power = gamepad1.right_trigger * direction
+            if (power > 0.05 || power < -0.05) {
+                currentMotor.power = power
+            } else {
+                currentMotor.power = 0.0
+            }
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestServo.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestServo.kt
@@ -9,51 +9,74 @@ import com.qualcomm.robotcore.hardware.Servo
 @TeleOp(name = "Test Servo", group = "Testing")
 class TestServo : CoreTeleOp() {
 
-    lateinit var servo: Servo
-
-    var servoName = "EDIT THIS TO CHOOSE YOUR SERVO NAME FROM THE CONFIG"
-    // you can find and edits configs in TeamCode/res/xml
-    // or in the Driver Station in the three dots menu > "configure robot"
+    lateinit var servos: List<Servo>
+    var currentServoIndex = 0
 
     val increasePos = Button({ gamepad1.right_bumper })
     val increasePosByALot = Button({ gamepad1.right_trigger > 0.5 })
     val decreasePos = Button({ gamepad1.left_bumper })
     val decreasePosByALot = Button({ gamepad1.left_trigger > 0.5 })
 
+    val nextServo = Button({ gamepad1.dpad_right })
+    val prevServo = Button({ gamepad1.dpad_left })
+
     override fun onInit() {
-        servo = hardwareMap[Servo::class.java, servoName]
-        servo.position = 0.5
+        servos = hardwareMap.getAll(Servo::class.java)
+        if (servos.isNotEmpty()) {
+            servos[currentServoIndex].position = 0.5
+        }
     }
 
     override fun onLoop() {
-        telemetryEverywhere.addLine("Init starts servo at 0.5")
+        if (servos.isEmpty()) {
+            telemetryEverywhere.addLine("[INFO] No servos found in config.")
+            telemetryEverywhere.addLine("To add a servo in your config, see the /TeamCode/res/xml folder" +
+            ", or, tap the three dot menu on your driver station and select \'Configure robot\'")
+            return
+        }
 
-        telemetryEverywhere.addLine("Increment by 0.01 == Right Bumper")
-        telemetryEverywhere.addLine("Increment by 0.1 == Right Trigger (>50%)")
+        if (nextServo.wasPressed()) {
+            currentServoIndex = (currentServoIndex + 1) % servos.size
+        }
+        if (prevServo.wasPressed()) {
+            currentServoIndex = (currentServoIndex - 1 + servos.size) % servos.size
+        }
 
-        telemetryEverywhere.addLine("Decrement by 0.01 == Left Bumper")
-        telemetryEverywhere.addLine("Decrement by 0.1 == Left Trigger (>50%)")
+        val currentServo = servos[currentServoIndex]
+        val servoName = hardwareMap.getNamesOf(currentServo).firstOrNull() ?: "Unknown"
 
-        telemetryEverywhere.addData("Servo Position", servo.position)
-        telemetryEverywhere.addData("Currently testing", servoName)
+        telemetryEverywhere.addLine("[INFO] This OpMode can find and test any servo in your config")
+        telemetryEverywhere.addLine("[INFO] Init starts servo at 0.5")
+
+        telemetryEverywhere.addLine("[INFO] Increment by 0.01 == Right Bumper")
+        telemetryEverywhere.addLine("[INFO] Increment by 0.1 == Right Trigger (>50%)")
+
+        telemetryEverywhere.addLine("[INFO] Decrement by 0.01 == Left Bumper")
+        telemetryEverywhere.addLine("[INFO] Decrement by 0.1 == Left Trigger (>50%)")
+
+        telemetryEverywhere.addLine("[INFO] Cycle through servos in your config == D-Pad Left/Right")
+        telemetryEverywhere.addLine("====================================")
+        telemetryEverywhere.addData("Currently testing", "SERVO ${currentServoIndex + 1}/${servos.size}, named ${servoName.uppercase()}")
+        telemetryEverywhere.addData("Servo Position", currentServo.position)
+        telemetryEverywhere.addLine("====================================")
 
         if (increasePos.wasPressed()) {
-            servo.position += 0.01
+            currentServo.position += 0.01
         }
 
         if (increasePosByALot.wasPressed()) {
-            servo.position = ((servo.position * 10).toInt() + 1) / 10.0
+            currentServo.position = ((currentServo.position * 10).toInt() + 1) / 10.0
         }
 
         if (decreasePos.wasPressed()) {
-            servo.position -= 0.01
+            currentServo.position -= 0.01
         }
 
         if (decreasePosByALot.wasPressed()) {
-            if ((servo.position * 10) == (servo.position * 10).toInt().toDouble()) {
-                servo.position -= 0.1
+            if ((currentServo.position * 10) == (currentServo.position * 10).toInt().toDouble()) {
+                currentServo.position -= 0.1
             }
-            servo.position = ((servo.position * 10).toInt()) / 10.0
+            currentServo.position = ((currentServo.position * 10).toInt()) / 10.0
         }
     }
 }
